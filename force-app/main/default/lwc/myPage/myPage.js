a7ff40079ddc464eb3e80edaec00caf6ba58c60d
Antoshyn4 <dimamar1003@gmail.com>
2022-12-14 13:08:52 +0000

    Tss

--- force-app/main/default/lwc/myPage/myPage.js
+++ force-app/main/default/lwc/myPage/myPage.js
@@ -1,9 +1,10 @@
-import { LightningElement, track, wire } from 'lwc';
+import { LightningElement, track, wire, api} from 'lwc';
 import getCustomSettings from '@salesforce/apex/ComponentCustomSettings.getCustomSettings';
 import getAccountList from '@salesforce/apex/ComponentCustomSettings.getAccountList';
 
 export default class MyPage extends LightningElement {
-    @track countOfColumns;
+    @track noSearchResult = false;
+    @api countOfColumns;
     @track countOfAllElements;
     @track countOfPageElements;
     @track countOfPages;
@@ -11,44 +12,116 @@ export default class MyPage extends LightningElement {
     @track error; 
     pageNum = 1;
 
-    @wire(getCustomSettings)
-    wiredCustomSettings({error, data}){
-        if (data) {
-            this.countOfAllElements = data.All_elements_count__c;
-            this.countOfColumns = data.Columns_count__c;
-            this.countOfPageElements = data.On_page_elements_count__c;
-            this.countOfPages = Math.ceil(this.countOfAllElements/this.countOfPageElements);
+    get countOfAllElementsAct(){
+        if (this.countOfAllElements > this.record.length) {
+            return  this.record.length;
         }
-        else if (error) {
-            alert("Error");
-        }
+        return this.countOfAllElements;
     }
-    @wire(getAccountList)
-    wiredList({error,data}){
-        if (data) {
+    get countOfPagesAct(){
+        if (this.countOfAllElements > this.record.length) {
+            return  Math.ceil(this.record.length/this.countOfPageElements);
+        }
+        return this.countOfPages;
+    }
+
+    connectedCallback(){
+        getCustomSettings()
+        .then(result => {
+            if (result.All_elements_count__c <= 0 || result.Columns_count__c <= 0 || result.On_page_elements_count__c < 0) {
+                alert('Incorrect input values');
+                // this.countOfAllElements = 0;
+                // this.countOfColumns = 0;
+                // this.countOfPageElements = 0;
+                // this.countOfPages = 0;
+                // this.record = undefined;
+            }
+            else {
+                this.countOfAllElements = result.All_elements_count__c;
+                this.countOfColumns = result.Columns_count__c;
+                this.countOfPageElements = result.On_page_elements_count__c;
+                this.countOfPages = Math.ceil(this.countOfAllElements/this.countOfPageElements);
+            }
+        }) 
+        .catch(error => {
+            alert('Error in getCS' + JSON.stringify(error));
+        });
+        getAccountList({isDesc: '', sortBy:})
+        .then(result => {
             this.record.length = 0;
             let temp = []; 
-            for (const key in data) {
-                temp.push(data[key])
+            for (const key in result) {
+                temp.push(result[key])
             }
             this.record = temp;
             this.error = undefined; 
-            if (this.countOfAllElements > this.record.length) {
-                this.countOfAllElements = this.record.length;
-                this.countOfPages = Math.ceil(this.countOfAllElements/this.countOfPageElements);
-            }
-            console.log("Ok");
-        }
-        else if (error){
+        })
+        .catch(error => {
             this.record = undefined;
             this.error = error;
-            console.log("Not Ok");
-            alert('Error');
-        }
+            alert('Error in getAcc' + JSON.stringify(error));
+        });
     }
+
     reDraw(event){
         this.countOfPages = Math.ceil(this.countOfAllElements/this.countOfPageElements);
         this.pageNum = event.detail;
         this.template.querySelector('c-list-of-accounts').draw(this.pageNum);
+    }
+    changeRecord(event){
+        if (event.detail['record'].length == 0 || event.detail['isAcc'] == null) {
+            this.noSearchResult = true;
+            this.record.length = 0;
+            this.template.querySelector('c-menu').changeCount(this.countOfPagesAct);
+            this.template.querySelector('c-list-of-accounts').changeRecord(event.detail);
+        }
+        else {
+            this.noSearchResult = false;
+            let data = event.detail['record'];
+            this.record = data;
+            this.error = undefined; 
+            this.template.querySelector('c-menu').changeCount(this.countOfPagesAct);
+            this.template.querySelector('c-list-of-accounts').changeRecord(event.detail);
+        }
+    }
+    fillAllValues(){
+        this.noSearchResult = false;
+        getCustomSettings()
+        .then(result => {
+            if (result.All_elements_count__c <= 0 || result.Columns_count__c <= 0 || result.On_page_elements_count__c < 0) {
+                alert('Incorrect input values');
+                // this.countOfAllElements = 0;
+                // this.countOfColumns = 0;
+                // this.countOfPageElements = 0;
+                // this.countOfPages = 0;
+                // this.record = undefined;
+            }
+            else {
+                this.countOfAllElements = result.All_elements_count__c;
+                this.countOfColumns = result.Columns_count__c;
+                this.countOfPageElements = result.On_page_elements_count__c;
+                this.countOfPages = Math.ceil(this.countOfAllElements/this.countOfPageElements);
+            }
+        }) 
+        .catch(error => {
+            alert('Error in getCS' + JSON.stringify(error));
+        });
+        getAccountList()
+        .then(result => {
+            debugger;
+            this.record.length = 0;
+            let temp = []; 
+            for (const key in result) {
+                temp.push(result[key])
+            }
+            this.record = temp;
+            this.error = undefined; 
+            this.template.querySelector('c-menu').changeCount(this.countOfPagesAct);
+        })
+        .catch(error => {
+            this.record = undefined;
+            this.error = error;
+            alert('Error in getAcc' + JSON.stringify(error));
+        });
     }
 }
