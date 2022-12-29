47a3cbedcb4c2f2d0947d86ff79432f4332a0285
Antoshyn4 <dimamar1003@gmail.com>
2022-12-29 10:06:33 +0000

    Aura

---
+++ force-app/main/default/aura/popUpComponent/popUpComponentController.js
@@ -0,0 +1,68 @@
+({
+    closeModel: function(component, event, helper) {
+       // Set isModalOpen attribute to false  
+       component.set("v.isModalOpen", false);
+    },
+    saveResults : function(component, event, helper) {
+       let a = event.getSource();
+       let value = a.get("v.value");
+       let field =  a.get("v.fieldName");
+
+       let updateRecordAction = component.get("c.upsertSObject");
+       let opt = component.get("v.options");
+       switch (field) {
+           case 'Phone':
+               value = value.replace('\ ','-');
+               value = value.replace(/^\-/,'-');
+               if (/[a-zA-Z]/g.test(value) || /^\-/.test(value) || /(?!\-)[^\w\s]/.test(value) || /^\-{1,99}/g.test(value)) {
+                  alert('Incorrect phone number');
+                  
+                  value = value.replace(/[a-zA-Z]/g,'');
+                  value = value.replace(/^\-/,'-');
+                  value = value.replace('\ ','-');
+                  value = value.replace(/(?!\-)[^\w\s]/,'');
+                  value = value.replace(/^\-{1,99}/g,''); 
+               }
+               opt[helper.check(opt, component, field)].value = value;
+               updateRecordAction.setParams({
+                  'fieldName' : field,
+                  'newValue' : value,
+                  'objectsId' : component.get("v.recordId")
+               });
+               component.set("v.options", opt);
+               $A.enqueueAction(updateRecordAction);
+               break;
+           case 'Fax':  
+               value = value.replace('\ ','-');
+               value = value.replace(/^\-/,'-');
+               if (/[a-zA-Z]/g.test(value) || /^\-/.test(value) || /(?!\-)[^\w\s]/.test(value) || /^\-{1,99}/g.test(value)) {
+                  alert('Incorrect phone number');
+                  
+                  value = value.replace(/[a-zA-Z]/g,'');
+                  value = value.replace(/^\-/,'-');
+                  value = value.replace('\ ','-');
+                  value = value.replace(/(?!\-)[^\w\s]/,'');
+                  value = value.replace(/^\-{1,99}/g,''); 
+               }
+               opt[helper.check(opt, component, field)].value = value;
+               updateRecordAction.setParams({
+                  'fieldName' : field,
+                  'newValue' : value,
+                  'objectsId' : component.get("v.recordId")
+               });
+               component.set("v.options", opt);   
+               $A.enqueueAction(updateRecordAction);
+               break;
+           default:
+               opt[helper.check(opt, component, field)].value = value;
+               updateRecordAction.setParams({
+                  'fieldName' : field,
+                  'newValue' : value,
+                  'objectsId' : component.get("v.recordId")
+               });
+               component.set("v.options", opt);   
+               $A.enqueueAction(updateRecordAction);
+               break;
+       }
+    },
+ })
